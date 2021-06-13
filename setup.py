6a1b791e50e8f6299e9c312c1cf3f5d4fee13142
Chaitanya Tata <chaitanya.tk17@gmail.com>
2021-06-14 01:55:58 +0530

    Fix the version query for tox
    `tox` does not have access to git environment, so, add some checks and
    use a default version.

--- setup.py
+++ setup.py
@@ -9,10 +9,19 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-# No error checking done here to keep this simple
-lat_tag_name = subprocess.check_output("git tag | tail -1", shell=True, encoding="UTF-8").strip()
-# Ignore numbers in the test (vx.y/testx.y)
-version = re.search(r"^[^0-9]+(.*)", lat_tag_name).group(1)
+lat_tag_name = ""
+# Only for tox where git is not available
+version = "1.0"
+try:
+    lat_tag_name = subprocess.check_output("git tag | tail -1", shell=True, encoding="UTF-8").strip()
+except CalledProcessError as ce:
+    print (ce)
+
+if lat_tag_name:
+    # Ignore numbers in the test (vx.y/testx.y)
+    version_m= re.search(r"^[^0-9]+(.*)", lat_tag_name)
+    if version_m:
+        version = version_m.group(1)
 
 setup(
     version=version,
