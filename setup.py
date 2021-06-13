4a106a95a4403803f88f779a36ba3b6e1b01b42a
Chaitanya Tata <chaitanya.tk17@gmail.com>
2021-06-14 01:27:34 +0530

    Automatic version based on latest tag
    Add support for automated version based on latest tag name, this avoid
    modifying the setup.* manually and just pushing a new tag should trigger
    a new package (also avoid conflicts in pushing to pypi).

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from setuptools import setup, find_packages
+import re
+import subprocess
 
+from setuptools import setup, find_packages
 # read the contents of your README file
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+# No error checking done here to keep this simple
+lat_tag_name = subprocess.check_output("git tag | tail -1", shell=True, encoding="UTF-8").strip()
+# Ignore numbers in the test (vx.y/testx.y)
+version = re.search(r"^[^0-9]+(.*)", lat_tag_name).group(1)
+
 setup(
+    version=version,
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
