910acb41429d4ce0f25fbac4e3e819941d4c1a76
Chaitanya Tata <chaitanya.tk17@gmail.com>
2021-06-14 00:33:15 +0530

    Add a Github workflow to publish packages to pypi
    This is based on [1], and uses pypa/gh-action-pypi-publish to push any
    tagged commits to pypi and pypitest servers. We enable this action only
    for tagged commits,
      - test* for pypitest (testing)
      - v* for pypi (release)
    
    Also add long_description to setup.py and make it read from README.md.
    
    [1] - https://packaging.python.org/guides/publishing-package-distribution-releases-using-github-actions-ci-cd-workflows/

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup, find_packages
 
-setup()
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
